daca370662e46fdf745eb05158883e6564e50aeb
ROBERT PATRICK CAREY III <rpc5102@psu.edu>
2019-12-03 22:40:14 -0500

    New fn to retrieve current path in app

--- R/util.R
+++ R/util.R
@@ -33,7 +33,7 @@ boastConnect <- function(session) {
   # Setup learning locker configuration
   config <- list(
     base_url = "https://learning-locker.stat.vmhost.psu.edu/",
-    auth = "Basic ZDQ2OTNhZWZhN2Q0ODRhYTU4OTFmOTlhNWE1YzBkMjQxMjFmMGZiZjo4N2IwYzc3Mjc1MzU3MWZkMzc1ZDliY2YzOTNjMGZiNzcxOThiYWU2",
+    auth = "Basic YWVlMjQ2ZDJmMzk2OWMwYTk0NTY3ZTQ0ZThiMDU3NDI3MjhhNWFiYjpmYWU4NDkwNTVlMzNiMDEyNzY0OGIyOGI5YzliZjI2NjMyYzFhYzJk",
     agent = rlocker::createAgent()
   )
 
@@ -41,4 +41,16 @@ boastConnect <- function(session) {
   connection <- rlocker::connect(session, config)
 
   return(connection)
+}
+
+# Gets page address from the current session
+#'@export
+getCurrentAddress <- function(session) {
+  return(paste0(
+    session$clientData$url_protocol, "//",
+    session$clientData$url_hostname,
+    session$clientData$url_pathname, ":",
+    session$clientData$url_port,
+    session$clientData$url_search
+  ))
 }
